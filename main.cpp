03913f75cec14214db3f627164f59708b1a4518a
Marcin Załęczny <mzaleczny@gmail.com>
2024-11-26 14:48:22 +0100

    Implementation of guessing in game loop.

--- main.cpp
+++ main.cpp
@@ -6,9 +6,9 @@
 #include "Drawings.h"
 using namespace std;
 
-enum class EHangmanState
+enum class EGameState
 {
-    EHS_None,
+    EHS_Start,
     EHS_Head,
     EHS_Torso,
     EHS_LeftHand,
@@ -16,14 +16,16 @@ enum class EHangmanState
     EHS_LeftLeg,
     EHS_RightLeg,
     EHS_Dead,
+    EHS_TopTen,
     EHS_Number
 };
 
-EHangmanState HangmanState = EHangmanState::EHS_None;
+EGameState HangmanState = EGameState::EHS_Start;
 std::vector<std::string> Phrases;
 std::string CurrentPhrase;
 std::string CurrentPhraseHint;
 std::string UserEnteredPhrase;
+std::string UserInput;
 TTerminalScreen Screen;
 bool DoGameLoop;
 
@@ -49,7 +51,6 @@ int main()
         GetInput();
         Update();
         Render();
-        DoGameLoop = false;
     }
 
     Deinitialize();
@@ -86,15 +87,50 @@ void PrintGameState()
     Screen.DrawClippedMultilineText(33, 8, HangmanRightHand);
     Screen.DrawClippedMultilineText(26, 15, HangmanLeftLeg);
     Screen.DrawClippedMultilineText(34, 15, HangmanRightLeg);
-    Screen.PresentScreen();
 }
 
 void GetInput()
 {
+    HANDLE hConsoleInput = GetStdHandle(STD_INPUT_HANDLE);
+    INPUT_RECORD Buffer;
+    DWORD NumberOfEventsRead = 0;
+    GetNumberOfConsoleInputEvents(hConsoleInput, &NumberOfEventsRead);
+    if (NumberOfEventsRead > 0)
+    {
+        if (ReadConsoleInput(hConsoleInput, &Buffer, 1, &NumberOfEventsRead))
+        {
+            if (Buffer.EventType == KEY_EVENT && Buffer.Event.KeyEvent.bKeyDown)
+            {
+                char ch = Buffer.Event.KeyEvent.uChar.AsciiChar;
+                if ((ch >= 'a' && ch <= 'z') || (ch >= 'A' && ch <= 'Z') || ch == ' ')
+                {
+                    UserInput = ch;
+                }
+                else if (Buffer.Event.KeyEvent.wVirtualKeyCode == 27)
+                {
+                    UserInput = "";
+                    DoGameLoop = false;
+                }
+            }
+        }
+    }
 }
 
 bool Update()
 {
+    if (UserInput != "")
+    {
+        char ch = std::tolower(UserInput[0]);
+        for (size_t i = 0; i < CurrentPhrase.length(); ++i)
+        {
+            if (std::tolower(CurrentPhrase[i]) == ch)
+            {
+                UserEnteredPhrase[i] = ch;
+            }
+        }
+        UserInput = "";
+    }
+    
     return false;
 }
 
@@ -105,8 +141,10 @@ void Render()
     PrintGameState();
 
     Screen.DrawText(50, 4, "Guess the phrase,  Hint: " + CurrentPhraseHint);
+    Screen.DrawText(52, 4, "Your phrase: [ " + CurrentPhrase + " ]");
     Screen.DrawText(52, 5, "Your phrase: [ " + UserEnteredPhrase + " ]");
     Screen.DrawText(52, 6, "Choose letter: ");
+    Screen.DrawText(52, 8, "Press ESC to break and quit the game");
     Screen.PresentScreen();
 }
 
