6d8a053df907524127358a39bce35503b01d55ac
Marcin Załęczny <mzaleczny@gmail.com>
2024-11-26 13:17:57 +0100

    Adding drawing objects for presenting Hangman hanged. Testing the full drawed shape.

---
+++ Drawings.h
@@ -0,0 +1,58 @@
+#pragma once
+
+std::string HangmanHead(R"(*****
+*****
+*****)");
+std::string HangmanTorso(R"( * 
+ * 
+ * 
+ * 
+ * 
+ * 
+***)");
+std::string HangmanLeftHand(R"(*
+  *
+    *
+      *
+        *)");
+std::string HangmanRightHand(R"(       *
+     *
+   *
+ *
+)");
+std::string HangmanLeftLeg(R"(    *
+   *
+  *
+ *
+*)");
+std::string HangmanRightLeg(R"(*
+ *
+  *
+   *
+    *)");
+std::string ScaffoldRoot(R"(*
+*
+*
+*
+*
+*
+*
+*
+*
+*
+*
+*
+*
+*
+*
+*
+*
+*
+*
+*
+*
+*)");
+std::string ScaffoldHorizontal(30, '*');
+std::string ScaffoldRope(R"(*
+*
+*)");
--- main.cpp
+++ main.cpp
@@ -2,11 +2,15 @@
 #include <string>
 #include <vector>
 #include "PhrasesFile.h"
+#include "TerminalScreen.h"
+#include "Drawings.h"
 using namespace std;
 
 std::vector<std::string> Phrases;
+TTerminalScreen Screen;
 
 bool Initialize();
+void PrintGameState();
 
 int main()
 {
@@ -16,10 +20,7 @@ int main()
         return -1;
     }
 
-    for (size_t i = 0; i < Phrases.size(); ++i)
-    {
-        cout << Phrases[i] << endl;
-    }
+    PrintGameState();
 
     return 0;
 }
@@ -32,4 +33,18 @@ bool Initialize()
         pf.ReadPhrases(Phrases);
     }
     return Phrases.size() > 0;
+}
+
+void PrintGameState()
+{
+    Screen.DrawClippedMultilineText(5, 2, ScaffoldRoot);
+    Screen.DrawClippedMultilineText(5, 2, ScaffoldHorizontal);
+    Screen.DrawClippedMultilineText(32, 2, ScaffoldRope);
+    Screen.DrawClippedMultilineText(30, 5, HangmanHead);
+    Screen.DrawClippedMultilineText(31, 8, HangmanTorso);
+    Screen.DrawClippedMultilineText(24, 8, HangmanLeftHand);
+    Screen.DrawClippedMultilineText(33, 8, HangmanRightHand);
+    Screen.DrawClippedMultilineText(26, 15, HangmanLeftLeg);
+    Screen.DrawClippedMultilineText(34, 15, HangmanRightLeg);
+    Screen.PresentScreen();
 }
